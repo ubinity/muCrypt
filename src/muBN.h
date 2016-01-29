53adba5a270b21e0a39f4020f7d2f4524b5a4de8
Cédric Mesnil <cedric.mesnil@ubinity.com>
2016-01-29 11:44:20 +0100

    Improve doc Set License

--- src/muBN.h
+++ src/muBN.h
@@ -1,12 +1,34 @@
+/*
+ *  Copyright 2015-2016, Ubinity SAS, cedric.mesnil@ubinity.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef muBN_H
 #define muBN_H
 
-#include "muBN_config.h"
-
 /*
  * muBN are big endian order of native word.
- * native word are either little or big endian
+ * Native word are either little or big endian
+ *
+ * A muBN is fixed precision number, represented in 2-complement
+ * plus a carry.
+ * 
+ * The carry is automaticcaly set depending on the operation.
  */
+
+
+#include "muBN_config.h"
 
 #if UBN_BITS_PER_WORD == 32
 #define UBN_LOG2_BITS_PER_WORD   4UL
@@ -131,35 +153,157 @@ enum {
 /* ======================================================================================= */
 /*                                 Logic Arithmetic                                        */
 /* ======================================================================================= */
+
+
+/**
+ * r = a ^b
+ * 
+ * @pre r,a,b have the word-length
+ *
+ * @param [out] r
+ * @param [in]  a
+ * @param [in]  b
+ *
+ * @spa
+ */
 void muBN_xor(muBN_t *r, muBN_t *a,  muBN_t *b);
+
+/**
+ * r = a | b
+ *
+ * @pre r,a,b have the word-length
+ *
+ * @param [out] r
+ * @param [in]  a
+ * @param [in]  b
+ *
+ * @spa
+ */
 void muBN_or (muBN_t *r, muBN_t *a,  muBN_t *b);
+
+/**
+ * r = a & b
+ *
+ * @pre r,a,b have the word-length
+ *
+ * @param [out] r
+ * @param [in]  a
+ * @param [in]  b
+ *
+ * @spa
+ */
 void muBN_and(muBN_t *r, muBN_t *a,  muBN_t *b);
+
+/**
+ * r = ~a 
+ *
+ * @pre r,a  have the word-length
+ *
+ * @param [out] r
+ * @param [in]  a
+ *
+ * @spa
+ */
 void muBN_not(muBN_t *r, muBN_t *a);
 
+/**
+ * Set to 'one' the specified bit. 
+ * Bit zero is the least significant one
+ * 
+ * @param [in/out] a
+ * @param [in]     n
+ *
+ * @spa
+ */
 void muBN_set_bit   (muBN_t *a, muBN_size_t  n);
+
+/**
+ * Set to 'zero' the specified bit. 
+ * Bit zero is the least significant one
+ * 
+ * @param [in/out] a
+ * @param [in]     n
+ *
+ * @spa
+ */
 void muBN_clear_bit (muBN_t *a, muBN_size_t  n);
+
+
+/**
+ * Test to 'zero' the specified bit. 
+ * Bit zero is the least significant one
+ * 
+ * @param [in]  a
+ * @param [in]  n
+ *
+ * @return 1 if bit is set to 'one', 0 else
+ *
+ * @spa
+ */
 muBN_word_t muBN_test_bit(const muBN_t *a, muBN_size_t n);
 
-void muBN_mask_bits (muBN_t *a, muBN_size_t  n);
-
+/**
+ * Set the carry to one
+ *
+ * @param [in/out] a
+ *
+ * @spa
+ */
 void muBN_setC(muBN_t *r);
+
+/**
+ * Set the carry to zero
+ *
+ * @param [in/out] a
+ *
+ * @spa
+ */
 void muBN_clearC(muBN_t *r);
+
+/**
+ * Test is carry is set
+ *
+ * @param [in] a
+ *
+ * @return 1 if carry is set, 0 else
+ *
+ * @spa
+ */
 muBN_word_t muBN_testC(muBN_t *r);
 
+/**
+ * Return the significant bit of 'a'. Carry is not considered.
+ * @param [in] a
+ *
+ * @return   significant bit length of 'a'
+ *
+ */
 muBN_size_t muBN_count_bit(const muBN_t *a);
 
 /* ======================================================================================= */
 /*                                Z Arithmetic                                             */
 /* ======================================================================================= */
 /**
- * compare unsigned BN in a fast way
+ * Compare unsigned BN in a fast way
+ *
+ * @pre a,b have the same word-length
+ *
+ * @param [in] a
+ * @param [in] b
+ *
  * @return  0  if  a = b
  * @return >0  if  a > b
  * @return <0  if  b > a
  */
 muBN_word_t muBN_ucmp(muBN_t *a,  muBN_t *b);
 /**
- * compare signed BN in a fast way
+ * Compare signed BN in a fast way
+ *
+ * @pre a,b have the same word-length
+ *
+ * @param [in] a
+ * @param [in] b
+ *
  * @return  0  if  a = b
  * @return >0  if  a > b
  * @return <0  if  b > a
@@ -167,40 +311,59 @@ muBN_word_t muBN_ucmp(muBN_t *a,  muBN_t *b);
 muBN_word_t muBN_cmp(muBN_t *a,  muBN_t *b);
 
 /**
- * Inplace r = -r 
+ * r = -r 
+ *
+ * @param [in/out] r
+ *
  */
 void muBN_negate(muBN_t *r);
 
 /**
- * compare signed BN to zero in a fast way
+ * Compare signed BN to zero in a fast way
+ *
+ * @param [in] a
+ * @param [in] b
+ * 
  * @return 1 if  a  = ZERO
  * @return 0 if  a != ZERO 
  */
 muBN_word_t muBN_is_zero(muBN_t *r); 
 
 /**
- * compare signed BN to one 
+ * Compare signed BN to one 
+ *
+ * @param [in] a
+ * @param [in] b
+ *
  * @return 1 if  a  = ONE
  * @return 0 if  a != ONE 
  */
 muBN_word_t muBN_is_one(muBN_t *r);
 
 /**
- * compare signed BN to zero
+ * Test is 'r' is even.
+ *
+ * @param [in] r
+ *
  * @return 1 if  a is even
  * @return 0 else
  */
-muBN_word_t   muBN_is_even(muBN_t *r);
+#define muBN_is_even(r) (!((r)->v[(r)->wlen-1]&1))
 
 /**
- * compare signed BN to zero
+ * Compare signed BN to zero
+ *
+ * @param [in] r
+ *
  * @return 1 if  odd
  * @return 0 else
  */
-muBN_word_t   muBN_is_odd (muBN_t *r); 
+#define muBN_is_odd(r) (((r)->v[(r)->wlen-1]&1))
 
 /**
  *  r= r >> n
+ *
+ * @param [in/out] r
  *
  * @spa
  */
@@ -208,7 +371,9 @@ void muBN_rshift  (muBN_t *r, muBN_size_t n);
 
 /**
  *  r= r >>> n
- *
+  *
+ * @param [in/out] r
+*
  * @spa
  */
 void muBN_urshift (muBN_t *r, muBN_size_t n); 
@@ -216,90 +381,143 @@ void muBN_urshift (muBN_t *r, muBN_size_t n);
 /**
  *  r= r >> 1
  *
+ * @param [in/out] r
+ *
  * @spa
  */
 void  muBN_rshift1 (muBN_t *r);
 
 /**
  *  r= r >>> 1
+  *
+ * @param [in/out] r
  *
  * @spa
  */
 void muBN_urshift1(muBN_t *r);
 
 /**
- *  r = r >>> n 
+ *  r = r >>> 1, and propagate carry if any 
  *  clear carry
+ *
+ * @param [in/out] r
  *
  * @spa
  */
 void  muBN_rshift1c(muBN_t *r);
 
 /**
- *  r= r >> n 
- *  set hight bit to last carry
+ *  r= r >> n, and propagate carry if any 
  *  clear carry
+ *
+ * @param [in/out] r
+ * @param [in]     n
  *
  * @spa
  */
 void  muBN_rshiftc (muBN_t *r, muBN_size_t  n);
 
 /**
- *  r= r << 1
+ *  r= r << 1, and propagate carry if any 
  *  clear carry
+ *
+ * @param [in/out] r
  *
  * @spa
  */
 void  muBN_lshift1(muBN_t *r);
 
 /**
- *  r= r << 1
+ *  r= r << 1,  and set carry if any 
+ *
+ * @param [in/out] r
  *
  * @spa
  */
 muBN_uword_t muBN_lshift1c(muBN_t *r);
 
 /**
- *  r= r << n
- *  clear carry
+ *  r= r << n,  and clear carry 
+ *
+ * @param [in/out] r
+ * @param [in]     n
  *
  * @spa
  */
 void  muBN_lshift (muBN_t *r, muBN_size_t  n);
 
 /**
- *  r= a + b
+ *  r= a + b, and set carry
+ *
+ * @pre r,a,b have the word-length
+ *
+ * @param [out] r
+ * @param [in] n
  *
  * @spa
  */
 muBN_word_t muBN_add(muBN_t *r,  muBN_t *a, muBN_t *b);
+
 /**
- *  r= a + b
+ *  r= a + b,  and set carry
+ *
+ * @pre r,a have the word-length
+ *
+ * @param [out] r
+ * @param [in] a
+ * @param [in] w
  *
  * @spa
  */
-muBN_word_t muBN_add_uword(muBN_t *r,  muBN_t *a, muBN_uword_t b);
+muBN_word_t muBN_add_uword(muBN_t *r,  muBN_t *a, muBN_uword_t w);
+
 /**
- *  r= a - b
+ *  r= a - b,  and set carry
+ *
+ * @pre r,a,b have the same word-length
+ *
+ * @param [out] r
+ * @param [in] a
+ * @param [in] b
  *
  * @spa
  */
 muBN_word_t   muBN_sub(muBN_t *r,  muBN_t *a, muBN_t *b);
+
 /**
- *  r=a - b
+ *  r=a - b,  and set carry
+ *
+ * @pre r,a have the same word-length
+ *
+ * @param [out] r
+ * @param [in] a
+ * @param [in] w
  *
  * @spa
  */
-muBN_word_t muBN_sub_uword(muBN_t *r,  muBN_t *a, muBN_uword_t b);
+muBN_word_t muBN_sub_uword(muBN_t *r,  muBN_t *a, muBN_uword_t w);
+
 /**
- *  r= a * b
+ *  r= a * b, and clear carry
+ *
+ * @pre r have length a lesat equal to twice the 'a' word-length + 'b' word-length + 1
+ *
+ * @param [out] r
+ * @param [in] a
+ * @param [in] b
  *
  * @spa
  */
 void muBN_mul(muBN_t *r, muBN_t *a, muBN_t *b);
 
 /**
- *  r= a * w
+ *  r= a * w, and clear carry
+ *
+ * @pre r have length a lesat equal to twice the 'a' word-length + 1
+ *
+ * @param [out] r
+ * @param [in]  a
+ * @param [in]  w
  *
  * @spa
  */
@@ -307,15 +525,22 @@ void  muBN_mul_uword(muBN_t *r, muBN_t *a, muBN_uword_t w);
 
 /**** secured function ****/ 
 /**
- * compare signed BN to zero 
+ * Compare signed BN to zero 
+ *
+ * @param [in] r
+ *
  * @return 1 if  a  = ZERO
  * @return 0 if  a != ZERO 
+ *
  * @spa
  */
 muBN_word_t   muBN_is_zero_sec(muBN_t *r);
 
 /**
- * compare signed BN to one 
+ * Compare signed BN to one 
+ *
+ * @param [in] r
+ *
  * @return 1 if  a  = ONE
  * @return 0 if  a != ONE 
  * @spa
@@ -323,22 +548,34 @@ muBN_word_t   muBN_is_zero_sec(muBN_t *r);
 muBN_word_t   muBN_is_one_sec(muBN_t *r);
 
 /**
- * compare unsigned BN
+ * Compare unsigned BN
+ *
+ * @param [in] a
+ * @param [in] b
+ *
  * @return  0 if  a = b
  * @return  1 if  a !=  b
+ *
  * @spa
  */
 muBN_word_t   muBN_ucmp_sec(muBN_t *a,  muBN_t *b );
 /**
- * compare unsigned BN
+ * Compare unsigned BN
+ *
+ * @param [in] a
+ * @param [in] b
+ *
  * @return  0 if  a = b
  * @return  1 if  a !=  b
+ *
  * @spa
  */
 muBN_word_t   muBN_cmp_sec(muBN_t *a,  muBN_t *b );
 
 /**
- *  randomize r
+ *  Randomize r
+ *
+ * @param [out] r
  */
 void  muBN_rand(muBN_t *r);
 
@@ -348,24 +585,68 @@ void  muBN_rand(muBN_t *r);
 /**
  * r= a % m
  *
+ * @pre r,m have the same word-length
+ *
  * @param r 
  * @param a 
  * @param m 
+ *
  * @param temp  temporary buffer with a word length a least equals to a.wlen*2 + 2 
  *
  */
 void muBN_mod(muBN_t *r,  muBN_t *a, muBN_t *m, muBN_uword_t  *tmp);
 
+/**
+ * r= a+b % m
+ *
+ * @pre r,a,b,m have the same word-length
+ * @pre a<m and b<m
+ *
+ * @param r 
+ * @param a 
+ * @param m 
+ *
+ */
 void  muBN_mod_add(muBN_t *r,  muBN_t *a, muBN_t *b, muBN_t *m);
+
+/**
+ * r= a-b % m
+ *
+ * @pre r,a,b,m have the same word-length
+ * @pre a<m and b<m
+ *
+ * @param r 
+ * @param a 
+ * @param m 
+ *
+ */
 void  muBN_mod_sub(muBN_t *r,  muBN_t *a, muBN_t *b, muBN_t *m);
+
+/**
+ * r= a*b % m
+ *
+ * @pre r,a,b,m have the same word-length
+ * @pre a<m and b<m
+ *
+ * @param r 
+ * @param a 
+ * @param m 
+ *
+ * @param temp  temporary buffer with a word length a least equals to a.wlen*2 + 2 
+ *
+ */
 void  muBN_mod_mul(muBN_t *r,  muBN_t *a, muBN_t *b, muBN_t *m,  muBN_uword_t *temp);
 
 /**
  * r = a⁻¹ mod m 
  *
+ * @pre m shall be odd
+ * @pre r,a,m have the same word-length
+ * @pre a<m and b<m
+ *
  * @param r 
  * @param a 
- * @param m     moduli, prime or not
+ * @param m     odd moduli, prime or not
  * @param temp  temporary buffer with a word length a least equals to m.wlen*4
  *
  * @return 1 if inverse exist
@@ -375,51 +656,88 @@ void  muBN_mod_mul(muBN_t *r,  muBN_t *a, muBN_t *b, muBN_t *m,  muBN_uword_t *t
 muBN_word_t muBN_mod_inv(muBN_t *r, muBN_t *a, muBN_t *m,  muBN_uword_t *temp) ;
 
 /*** Secured function ***/
-/*
- * r = a+b mod m 
+/**
+ * r= a+b % m
+ *
+ * @pre r,a,b,m have the same word-length
+ * @pre a<m and b<m
+ *
+ * @param r 
+ * @param a 
+ * @param m 
+ * @param temp  temporary buffer with a word length a least equals to a.wlen
+ *
  */
 void  muBN_mod_add_sec(muBN_t *r,  muBN_t *a, muBN_t *b, muBN_t *m, muBN_uword_t  *tmp);
-/*
- * r = a-b mod m 
+
+/**
+ * r= a-b % m
+ *
+ * @pre r,a,b,m have the same word-length
+ * @pre a<m and b<m
+ *
+ * @param r 
+ * @param a 
+ * @param m 
+ * @param temp  temporary buffer with a word length a least equals to a.wle
+ *
  */
 void  muBN_mod_sub_sec(muBN_t *r,  muBN_t *a, muBN_t *b, muBN_t *m, muBN_uword_t  *tmp);
 
 
 /* ======================================================================================= */
-/*                                Mongomery  Arithmetic                                    */
+/*                                Montgomery  Arithmetic                                    */
 /* ======================================================================================= */
 
 /**
- * j0 = 
- * j1 = 
+ * Compute the two Montgomery constants for the given modulus:
+ * . J0 = -m mod 2^b   (b, muBN_word bit length)
+ * . J1 = 2^2l mod m   (l, modulus bit length)
+ * 
  *
- * @param m 
- * @param j1 
- * @param temp  temporary buffer with a word length a least equals to m.wlen*5 + 4
- *                                                                    
+ * @pre j1,m have the same word-length
+ *
+ * @param [in]  m     modulus
+ * @param [out] j1    where to store J1
+ * @param [in]  temp  temporary buffer with a word length a least equals to m.wlen*5 + 4
+ *
+ * @return j0                                                                   
  *
  * @spa
  */
 muBN_uword_t muBN_mgt_cst( muBN_t *m, muBN_t *j1, muBN_uword_t *temp);
 
 /**
- *  r = Mont⁻¹(a), convert a number from  Montgomery representation into Z/pZ space.
- */
+ *  r = Mont⁻¹(a), convert a number from  Montgomery representation into Z/mZ space.
+ *
+ * @param [in]  m     modulus
+ * @param [out] j1    where to store J1
+ * @param [in]  temp  temporary buffer with a word length a least equals to m.wle */
 void muBN_mgt_mgt2z(muBN_t *r,  muBN_t *a, muBN_t *m, muBN_t *one, muBN_uword_t j0);
 
 /**
- *  r = Mont(a),  convert a number from  Z/pZ space into Montgomery representation. 
+ *  r = Mont(a),  convert a number from  Z/mZ space into Montgomery representation. 
+ * @pre mr,a,m have the same word-length
+ * @pre ma<m 
+ *
+ * @param a
+ * @param b
+ * @param m
+ * @param j0
  * 
  */
-void muBN_mgt_z2mgt(muBN_t *r,  muBN_t *a, muBN_t *m, muBN_t *j1, muBN_uword_t j0);
+void muBN_mgt_z2mgt(muBN_t *mr,  muBN_t *a, muBN_t *m, muBN_t *j1, muBN_uword_t j0);
 
 /**
  * mr = MultMont(ma,mb),  with r = a*b mod m
  *
- * ma is the Mongomery representetation of a: ma = a.R mod m
- * mb is the Mongomery representetation of b: mb = b.R mod m
- * mr is the Mongomery representetation of r: mr = r.R mod m
-
+ * ma is the Montgomery representetation of a: ma = a.R mod m
+ * mb is the Montgomery representetation of b: mb = b.R mod m
+ * mr is the Montgomery representetation of r: mr = r.R mod m
+ *
+ * @pre mr,ma,mb,m have the same word-length
+ * @pre ma<m  and m <m
+ *
  * @param a
  * @param b
  * @param m
@@ -427,16 +745,18 @@ void muBN_mgt_z2mgt(muBN_t *r,  muBN_t *a, muBN_t *m, muBN_t *j1, muBN_uword_t j
  *
  * @spa
  */
-void muBN_mgt_mul(muBN_t *r,  muBN_t *a, muBN_t *b, muBN_t *m,  muBN_uword_t j0);
+void muBN_mgt_mul(muBN_t *mr,  muBN_t *ma, muBN_t *mb, muBN_t *m,  muBN_uword_t j0);
 
 
 /**
  *  mr = ma⁻¹ mod m,  with r = a⁻¹ mod m 
  * 
- * ma is the Mongomery representetation of a: ma = a.R mod m
- * mr is the Mongomery representetation of r: mr = r.R mod m
- *  
-  *
+ * ma is the Montgomery representetation of a: ma = a.R mod m
+ * mr is the Montgomery representetation of r: mr = r.R mod m
+ *
+ * @pre mr,ma,m have the same word-length
+ * @pre ma<m 
+ *
  * @param mr
  * @param ma
  * @param m
@@ -445,10 +765,13 @@ void muBN_mgt_mul(muBN_t *r,  muBN_t *a, muBN_t *b, muBN_t *m,  muBN_uword_t j0)
  * @param temp temporary buffer with a word length a least equals to m.wlen*3
  *
  */
-muBN_uword_t muBN_mgt_inv(muBN_t *mr, muBN_t *ma, muBN_t *m,  muBN_uword_t j0, muBN_t *j1,
+muBN_uword_t muBN_mgt_inv(muBN_t *mr, muBN_t *ma, muBN_t *m,  
                         muBN_uword_t *temp);
 /**
- * r = a * b mod m , using two montgomery multiplications
+ * r = a * b mod m , using two Montgomery multiplications
+ *
+ * @pre r,a,b,m have the same word-length
+ * @pre a<m and b<m
  *
  * @param a
  * @param b
